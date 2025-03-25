789c8512a6808d8e0fb2adb841e085795890ce40
Adi Jain <jainadi1717@gmail.com>
2025-03-25 20:46:38 +0530

    added cors for render

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,8 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({
+    origin: ["http://localhost:5173", "https://ace-pickleball-web-app.onrender.com"], credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
